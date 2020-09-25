dfe641b39188371a6395d6e50bd147ede71f677d
yassin-asu <yassinabda@hotmail.com>
2020-09-25 15:29:00 +0200

    task1

---
+++ task1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int x, y;
+    printf("please enter value of x:");
+    scanf("%d",&x);
+     printf("please enter value of y:");
+    scanf("%d",&y);
+    printf("the value of x is %d \n the value of y is %d",x,y);
+    printf("\n the multiplication value is: %d", x*y);
+     printf("\n the division value is: %d", x/y);
+     printf("\n the sum value is: %d", x+y);
+      printf("\n the difference value is: %d", x-y);
+       printf("\n the remainder value is: %d", x%y);
+
+return 0;
+
+}
